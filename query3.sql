2f53b433f932c15ed33ec93cf8f564a8adac8a52
federus1105 <federusrudi@gmail.com>
2025-10-29 22:42:18 +0700

    feat answer for question number three and four

--- query3.sql
+++ query3.sql
@@ -11,6 +11,19 @@ HAVING count(roles.role) > 5
 ORDER BY amount_role DESC
 LIMIT 10
 ;
+--3 
+SELECT directors.first_name, COUNT(movies_directors.movie_id) AS amount_movie FROM directors
+JOIN movies_directors ON movies_directors.director_id = directors.id
+GROUP BY directors.first_name, directors.id
+ORDER BY amount_movie DESC 
+LIMIT 10;
+
+-- 4
+SELECT year, count(year) as jumlahnya
+FROM movies
+GROUP BY year
+ORDER BY jumlahnya DESC
+LIMIT 1;
 
 -- 5
 SELECT movies.name, string_agg(movies_genres.genre, ', ') AS "Genre" FROM movies
