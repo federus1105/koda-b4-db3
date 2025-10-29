1fd8c38a70110db2f40f7849f925b00fe5426104
federus1105 <federusrudi@gmail.com>
2025-10-29 18:34:31 +0700

    Feat answer for question 3

--- query3.sql
+++ query3.sql
@@ -2,3 +2,12 @@
 SELECT directors.first_name, count(directors_genres.genre) amount_genre FROM directors
 JOIN directors_genres ON directors_genres.director_id = directors.id
 GROUP BY directors.first_name, directors.id ORDER BY amount_genre DESC;
+
+--2 
+SELECT actors.first_name, count(roles.role) amount_role FROM actors
+JOIN roles ON roles.actor_id = actors.id
+GROUP BY actors.first_name, roles.role
+HAVING count(roles.role) > 5
+ORDER BY amount_role DESC
+LIMIT 10
+;
