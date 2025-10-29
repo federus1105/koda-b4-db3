1b9579a7c494e1352ab41de69e01244f2ebbf132
federus1105 <federusrudi@gmail.com>
2025-10-29 11:20:55 +0700

    Fix (query1 ) : condition on betweem

--- query1.sql
+++ query1.sql
@@ -2,7 +2,7 @@
 SELECT year from movies WHERE year > 2000; 
 
 -- Mencari nama akhiran  s
-SELECT first_name, last_name FROM actors WHERE first_name LIKE '%s' OR last_name LIKE '%s';
+SELECT first_name, last_name FROM actors WHERE first_name LIKE '%s' AND last_name LIKE '%s';
 
 -- mencari rating di antar 5 dan 7, dan tahun rilis diantara 2004 dan 2006
 SELECT rankscore, year FROM movies WHERE rankscore BETWEEN 5 AND 7 AND year BETWEEN 2004 AND 2006; 
